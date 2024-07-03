eee163655600995ee89d8217ec98f7d9216b2fbe
Yuchuan Tao <taoyuchuan@gmail.com>
2024-07-03 16:51:14 -0400

    Create layout page.

--- react-client/src/pages/Layout/index.js
+++ react-client/src/pages/Layout/index.js
@@ -1,5 +1,60 @@
+import './index.scss';
+import { Card, Col, Row } from 'antd';
+import Plot from 'react-plotly.js';
+
 const Layout = () => {
-  return <div>This is layout page.</div>;
+  return (
+    <div className="layout">
+      <Row className="layout-container">
+        <Col span={12}>
+          <Card>
+            <Plot
+              data={[
+                {
+                  x: [1, 2, 3],
+                  y: [2, 6, 3],
+                  type: 'scatter',
+                  mode: 'lines+markers',
+                  marker: {color: 'red'},
+                },
+                {
+                  x: [1, 2, 3],
+                  y: [5, 9, 7],
+                  type: 'scatter',
+                  mode: 'lines+markers',
+                  marker: {color: 'blue'},
+                },
+              ]}
+              layout={ {width: 600, height: 400, title: 'A Fancy Plot'} }
+            />
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card>
+            <Plot
+              data={[
+                {
+                  x: [1, 2, 3],
+                  y: [2, 6, 3],
+                  type: 'scatter',
+                  mode: 'lines+markers',
+                  marker: {color: 'red'},
+                },
+                {
+                  x: [1, 2, 3],
+                  y: [5, 9, 7],
+                  type: 'scatter',
+                  mode: 'lines+markers',
+                  marker: {color: 'blue'},
+                },
+              ]}
+              layout={ {width: 600, height: 400, title: 'A Fancy Plot'} }
+            />
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Layout;

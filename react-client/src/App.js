0539f605cade6d7abb6d205e2bb1aa7f091f6936
Yuchuan Tao <taoyuchuan@gmail.com>
2024-07-03 14:43:14 -0400

    Add Ant Design dependency

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,5 +1,12 @@
+import { Button } from 'antd';
+
 function App() {
-  return <div>This is App.</div>
+  return (
+    <div>
+      This is App.
+      <Button type="primary">Button</Button>
+    </div>
+  );
 };
 
 export default App;

5f326ba261b47e71db251b101ac0be592ae581aa
Yuchuan Tao <taoyuchuan@gmail.com>
2024-07-03 23:05:46 -0400

    Add login endpoint.

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,39 @@
 from flask import Flask
+from flask import request
+from flask_cors import CORS, cross_origin
+
+import numpy as np
 
 app = Flask(__name__)
+CORS(app)
 
 # Data API route
-@app.route("/data")
-def getData():
-    return { "data": [[5.1, 3.5, 1.4, 0.2, "Iris-setosa"], [4.9, 3, 1.4, 0.2, "Iris-setosa"], [4.7, 3.2, 1.3, 0.2, "Iris-setosa"], [4.6, 3.1, 1.5, 0.2, "Iris-setosa"]] }
+@app.route("/data/<email>")
+@cross_origin()
+def getData(email):
+    arr = np.genfromtxt("iris.csv", delimiter=",", dtype=None)
+    if email == "setosa@gmail.com":
+        arr = [x for x in arr if x[4] == "Iris-setosa"]
+    elif email == "virginica@gmail.com":
+        arr = [x for x in arr if x[4] == "Iris-virginica"]
+    transposed_array = np.transpose(arr)
+    print(email)
+    return { "data": { "sepalLength": list(transposed_array[0]), "sepalWidth": list(transposed_array[1]), "petalLength": list(transposed_array[2]), "petalWidth": list(transposed_array[3])} }
+
+@app.route("/login", methods=["POST"])
+@cross_origin()
+def login():
+    print(request.json)
+    email = request.json.get('email')
+    password = request.json.get('password')
+    print(email)
+    print(password)
+    if email == "setosa@gmail.com" and password == "setosa":
+        return "setosa_token"
+    elif email == "virginica@gmail.com" and password == "virginica":
+        return "virginica_token"
+    else:
+        return ""
 
 if __name__ == "__main__":
     app.run(debug=True)
